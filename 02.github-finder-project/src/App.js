9c894e1ae5636637f09535c916cd43b73dbb0fde
Yavor Petkov <hermsues@gmail.com>
2021-07-01 11:47:45 +0300

    intro to react

---
+++ 02.github-finder-project/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// with a function
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			<h1>Hello From REACT</h1>
+// 		</div>
+// 	);
+// }
+
+// with a class
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>Hello From REACT</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
