925adc7d2353278070b2764cee6c9ad4d3dba7d6
Yavor Petkov <hermsues@gmail.com>
2021-07-06 14:50:30 +0300

    added a home page and a not found page

--- 02.github-finder-project/src/App.js
+++ 02.github-finder-project/src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 // npm i react-router-dom
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -30,19 +30,12 @@ const App = () => {
 						<div className="container">
 							<Alert />
 							<Switch>
-								<Route
-									exact
-									path="/"
-									render={() => (
-										<Fragment>
-											<Search />
-											<Users />
-										</Fragment>
-									)}
-								/>
-								{/* a path to a difrent page */}
+								{/* a path to a page */}
+								<Route exact path="/" component={Home} />
 								<Route exact path="/about" component={About} />
 								<Route exact path="/user/:login" component={User} />
+								{/* has to be at the end of the routes */}
+								<Route component={NotFound} />
 							</Switch>
 						</div>
 					</div>
