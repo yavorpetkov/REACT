2702392a9a99071b50630a8fe3a3d0fad3acc931
Yavor Petkov <hermsues@gmail.com>
2021-07-07 12:12:06 +0300

    Connected MongoDB ot the app

--- 03.contact-keeper/server.js
+++ 03.contact-keeper/server.js
@@ -1,7 +1,11 @@
 // common js
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.json({ msg: `Welcome to the ContactKeeper API...` }));
 
