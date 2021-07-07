9ef8f77be4d61689117d0ad92074aec6950f4a23
Yavor Petkov <hermsues@gmail.com>
2021-07-07 16:25:58 +0300

    Hashing the passwords

--- 03.contact-keeper/routes/users.js
+++ 03.contact-keeper/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -14,13 +15,39 @@ router.post(
 		check('email', 'Please include a valid email').isEmail(),
 		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
 	],
-	(req, res) => {
+	async (req, res) => {
 		// do this for routes that accept data
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		res.send('passed');
+
+		const { name, email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email: email });
+
+			if (user) {
+				return res.status(400).json({ msg: 'User already exists' });
+			}
+
+			user = new User({
+				name,
+				email,
+				password
+			});
+
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			res.send('User Saved');
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
 	}
 );
 
