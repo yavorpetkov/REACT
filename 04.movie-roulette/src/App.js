b6fd14b145ffca0189651557b49cf88dd1c792c0
Yavor Petkov <hermsues@gmail.com>
2021-07-08 15:41:16 +0300

    added react router dom

--- 04.movie-roulette/src/App.js
+++ 04.movie-roulette/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 
-function App() {
+const App = () => {
 	return (
 		<div className="App">
 			<Navbar />
 		</div>
 	);
-}
+};
 
 export default App;
