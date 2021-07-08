c9020d50644962b030e11fed8f0d8b6c025e1751
Yavor Petkov <hermsues@gmail.com>
2021-07-08 15:28:08 +0300

    added some components

---
+++ 04.movie-roulette/src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<nav>
+			<h1>
+				<strong>netflix</strong>roulette
+			</h1>
+			<a href="/">+ ADD MOVIE</a>
+		</nav>
+	);
+};
+
+export default Navbar;
