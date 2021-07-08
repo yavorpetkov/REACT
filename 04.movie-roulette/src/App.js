cd0f6f2b69b571669e0776d91f673c0a77fa8c08
Yavor Petkov <hermsues@gmail.com>
2021-07-08 15:42:42 +0300

    added react router dom

--- 04.movie-roulette/src/App.js
+++ 04.movie-roulette/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import './App.css';
