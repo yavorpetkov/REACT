da154afc8c5266841f5bb0660097bb5856dc51d5
Yavor Petkov <hermsues@gmail.com>
2021-07-15 11:54:43 +0300

    contact context and the global state

---
+++ 03.contact-keeper/client/src/context/contact/ContactState.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-unused-vars */
+import React, { useReducer, userReduser } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+	ADD_CONTACT,
+	DELETE_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRNET,
+	UPDATE_CONTACT,
+	FILTER_CONTACTS,
+	CLEAR_FILTER
+} from '../types';
+
+const ContactState = (props) => {
+	const initialState = {
+		contacts: [
+			{
+				id: 1,
+				name: 'Jill Johnson',
+				email: 'jill@gmail.com',
+				phone: '111-111-1111',
+				type: 'personal'
+			},
+			{
+				id: 2,
+				name: 'Sara Watson',
+				email: 'sara@gmail.com',
+				phone: '222-222-2222',
+				type: 'personal'
+			},
+			{
+				id: 3,
+				name: 'Harry White',
+				email: 'harry@gmail.com',
+				phone: '333-333-3333',
+				type: 'professional'
+			}
+		]
+	};
+
+	const [ state, dispatch ] = useReducer(contactReducer, initialState);
+
+	// Add Contact
+
+	// Delete Contact
+
+	// Set Currnet Contact
+
+	// Clear Currnet Contact
+
+	//  Update Contact
+
+	// Filter Contacts
+
+	// Clear Filter
+
+	return (
+		<ContactContext.Provider
+			value={{
+				contacts: state.contacts
+			}}
+		>
+			{props.children}
+		</ContactContext.Provider>
+	);
+};
+
+export default ContactState;
