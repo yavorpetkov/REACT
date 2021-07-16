559f67272c754a847f0b1177ef48a29ebf92c1e8
Yavor Petkov <hermsues@gmail.com>
2021-07-16 16:06:19 +0300

    edit and update contact action

--- 03.contact-keeper/client/src/components/contacts/ContactForm.js
+++ 03.contact-keeper/client/src/components/contacts/ContactForm.js
@@ -1,8 +1,28 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
 	const contactContext = useContext(ContactContext);
+
+	const { addContact, current, clearCurrent, updateContact } = contactContext;
+
+	// mymics the effect of componentDidMount
+	useEffect(
+		() => {
+			if (current !== null) {
+				setContact(current);
+			} else {
+				setContact({
+					name: '',
+					email: '',
+					phone: '',
+					type: 'personal'
+				});
+			}
+		},
+		// only happens when these to are changed
+		[ contactContext, current ]
+	);
 
 	const [ contact, setContact ] = useState({
 		name: '',
@@ -13,12 +33,17 @@ const ContactForm = () => {
 
 	const { name, email, phone, type } = contact;
 
+	// FUNCTIONS
 	// e.target.name will look at the name and the value and if they match it will set it
 	const onChange = (e) => setContact({ ...contact, [e.target.name]: e.target.value });
 
 	const onSubmit = (e) => {
 		e.preventDefault();
-		contactContext.addContact(contact);
+		if (current === null) {
+			addContact(contact);
+		} else {
+			updateContact(contact);
+		}
 		// returns everything back to normal
 		setContact({
 			name: '',
@@ -28,9 +53,13 @@ const ContactForm = () => {
 		});
 	};
 
+	const clearAll = () => {
+		clearCurrent();
+	};
+
 	return (
 		<form onSubmit={onSubmit}>
-			<h2 className="text-primary">Add Contact</h2>
+			<h2 className="text-primary">{current ? 'Edit Contact' : 'Add Contact'}</h2>
 			<input type="text" placeholder="Name" name="name" value={name} onChange={onChange} />
 			<input type="email" placeholder="Email" name="email" value={email} onChange={onChange} />
 			<input type="text" placeholder="Phone" name="phone" value={phone} onChange={onChange} />
@@ -46,8 +75,19 @@ const ContactForm = () => {
 			/>{' '}
 			Professional
 			<div>
-				<input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
+				<input
+					type="submit"
+					value={current ? 'Update Contact' : 'Add Contact'}
+					className="btn btn-primary btn-block"
+				/>
 			</div>
+			{current && (
+				<div>
+					<button className="btn btn-light btn-block" onClick={clearAll}>
+						Clear
+					</button>
+				</div>
+			)}
 		</form>
 	);
 };
--- 03.contact-keeper/client/src/context/contact/contactReducer.js
+++ 03.contact-keeper/client/src/context/contact/contactReducer.js
@@ -17,6 +17,11 @@ export default (state, action) => {
 				...state,
 				contacts: [ ...state.contacts, action.payload ]
 			};
+		case UPDATE_CONTACT:
+			return {
+				...state,
+				contacts: state.contacts.map((contact) => (contact.id === action.payload.id ? action.payload : contact))
+			};
 		case DELETE_CONTACT:
 			return {
 				...state,
@@ -32,6 +37,7 @@ export default (state, action) => {
 				...state,
 				current: null
 			};
+
 		default:
 			return state;
 	}
