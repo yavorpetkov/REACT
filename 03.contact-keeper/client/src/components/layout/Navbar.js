0e1b6922df4cc6df99e88346d5df30d976785c30
Yavor Petkov <hermsues@gmail.com>
2021-07-19 13:35:00 +0300

    added a link to the home page from the logo

--- 03.contact-keeper/client/src/components/layout/Navbar.js
+++ 03.contact-keeper/client/src/components/layout/Navbar.js
@@ -4,27 +4,28 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
 	return (
-		<div className="bg-primary">
-			<div className="navbar container">
-				<h1>
-					<i className={icon}> {title}</i>
-				</h1>
+		<div className="navbar bg-primary">
+			<h1>
+				<a href="/" className={icon}>
+					{' '}
+					{title}
+				</a>
+			</h1>
 
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/about">About</Link>
-					</li>
-					<li>
-						<Link to="/register">Register</Link>
-					</li>
-					<li>
-						<Link to="/login">Login</Link>
-					</li>
-				</ul>
-			</div>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+				<li>
+					<Link to="/register">Register</Link>
+				</li>
+				<li>
+					<Link to="/login">Login</Link>
+				</li>
+			</ul>
 		</div>
 	);
 };
