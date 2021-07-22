a11d606834ddb7492a16577fb63ad658a379529e
Yavor Petkov <hermsues@gmail.com>
2021-07-22 14:39:27 +0300

    Preloader component added

---
+++ 04.it-logger/src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => {
+	return (
+		<div className="progress blue lighten-4">
+			<div className="indeterminent blue" />
+		</div>
+	);
+};
+
+export default Preloader;
