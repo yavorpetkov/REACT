f7f1033124b7a8ddbbc2ab8be4cdf23b89385bfe
Yavor Petkov <hermsues@gmail.com>
2021-07-24 18:10:41 +0300

    AddLogModal component

---
+++ 04.it-logger/src/components/logs/AddLogModal.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+const AddLogModal = () => {
+	const [ message, setMessage ] = useState('');
+	const [ attention, setAttention ] = useState(false);
+	const [ tech, setTech ] = useState('');
+
+	return (
+		<div id="add-log-modal" className="modal" style={modalStyle}>
+			<div className="modal-content">
+				<h4>Enter System Log</h4>
+				<div className="row">
+					<div className="input-field">
+						<input
+							type="text"
+							name="message"
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+						/>
+						<label htmlFor="message" className="active">
+							Log Message
+						</label>
+					</div>
+				</div>
+				<div className="row">
+					<div className="input-field">
+						<select
+							name="tech"
+							value={tech}
+							className="browser-default"
+							onChange={(e) => setTech(e.target.value)}
+						>
+							<option value="" disabled>
+								Select Technitian
+							</option>
+							<option value="John Doe">John Doe</option>
+							<option value="Sam Smith">Sam Smith</option>
+							<option value="Sara Wilson">Sara Wilson</option>
+						</select>
+					</div>
+				</div>
+				<div className="row">
+					<div className="input-field">
+						<p>
+							<label>
+								<input
+									type="checkbox"
+									className="filled-in"
+									value={attention}
+									checked={attention}
+									onChange={(e) => setAttention(!attention)}
+								/>
+							</label>
+							<span>Needs Attention</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const modalStyle = {
+	width: '75%',
+	height: '75%'
+};
+
+export default AddLogModal;
