f1564410580b6e658d25a1e9bd1cb1f0e45ea2eb
Yavor Petkov <hermsues@gmail.com>
2021-07-30 18:36:53 +0300

    Did some cleanup

--- 05.react-project-1/src/App.js
+++ 05.react-project-1/src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
 	return (
 		<div className="App">
 			<header className="App-header" />
 		</div>
 	);
-}
+};
 
 export default App;
