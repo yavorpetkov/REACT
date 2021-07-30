c3099d5b95310ae2389d09c45449397522b87b94
Yavor Petkov <hermsues@gmail.com>
2021-07-30 18:50:09 +0300

    Navbar component

---
+++ 05.react-project-1/src/components/layouts/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<div>
+			<ul>
+				<li>
+					<a href="#!">Home</a>
+				</li>
+				<li>
+					<a href="#!">About</a>
+				</li>
+				<li>
+					<a href="#!">Contact</a>
+				</li>
+			</ul>
+			<h1>
+				GOLDEN<br />
+				RAMEN
+			</h1>
+			<ul>
+				<li>
+					<a href="#!">Home</a>
+				</li>
+				<li>
+					<a href="#!">About</a>
+				</li>
+				<li>
+					<a href="#!">Contact</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Navbar;
