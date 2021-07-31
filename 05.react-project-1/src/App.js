cf8ea3afb2d8d57a68e2a75bc98f60e27350344a
Yavor Petkov <hermsues@gmail.com>
2021-07-31 09:25:48 +0300

    Added some CSS

--- 05.react-project-1/src/App.js
+++ 05.react-project-1/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/layouts/Navbar';
 import './App.css';
 
 const App = () => {
 	return (
 		<div className="App">
-			<header className="App-header" />
+			<Navbar />
 		</div>
 	);
 };
