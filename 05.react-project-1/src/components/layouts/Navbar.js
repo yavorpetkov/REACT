6da2fe920557183fa5533b49780d975b580aa4ca
Yavor Petkov <hermsues@gmail.com>
2021-08-02 17:42:49 +0300

    fixed the css a little

--- 05.react-project-1/src/components/layouts/Navbar.js
+++ 05.react-project-1/src/components/layouts/Navbar.js
@@ -3,24 +3,26 @@ import React from 'react';
 const Navbar = () => {
 	return (
 		<div className="container">
-			<div className="text-left">
-				<a href="#!">Home</a>
+			<nav className="navbar">
+				<ul>
+					<a href="#!">Home</a>
 
-				<a href="#!">About</a>
+					<a href="#!">About</a>
 
-				<a href="#!">Contact</a>
-			</div>
-			<h1 className="text-center">
-				GOLDEN<br />
-				RAMEN
-			</h1>
-			<div className="text-right">
-				<a href="#!">Home</a>
+					<a href="#!">Contact</a>
+				</ul>
+				<h1 style={{ lineHeight: '1.4rem' }}>
+					GOLDEN<br />
+					RAMEN
+				</h1>
+				<ul>
+					<a href="#!">Home</a>
 
-				<a href="#!">About</a>
+					<a href="#!">About</a>
 
-				<a href="#!">Contact</a>
-			</div>
+					<a href="#!">Contact</a>
+				</ul>
+			</nav>
 		</div>
 	);
 };
