99928c9f56c3fa06c47f46a94b5fcd2f55f26a8c
Yavor Petkov <hermsues@gmail.com>
2021-08-03 12:29:23 +0300

    Header

--- 05.react-project-1/src/components/layouts/Header.js
+++ 05.react-project-1/src/components/layouts/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import Navbar from './Navbar';
+import Navbar from './items/Navbar';
 
 const Header = () => {
 	return (
 		<div className="header">
-			<Navbar />
+			<div className="container">
+				<Navbar />
+				<div className="header-text">
+					<div>
+						<h2>
+							It's that<br />
+							simple.
+						</h2>
+						<h4>FEATURING OUR NEW SPRING SPECIALS!</h4>
+						<a href="#!">Details</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ 05.react-project-1/src/components/layouts/items/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<ul>
+				<a href="#!">Home</a>
+
+				<a href="#!">About</a>
+
+				<a href="#!">Contact</a>
+			</ul>
+			<h1 style={{ lineHeight: '1.4rem' }}>
+				GOLDEN<br />
+				RAMEN
+			</h1>
+			<ul>
+				<a href="#!">Home</a>
+
+				<a href="#!">About</a>
+
+				<a href="#!">Contact</a>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
