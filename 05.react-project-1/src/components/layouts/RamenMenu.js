2920fb032827f2c51639dc2c2d9396a38a8761bd
Yavor Petkov <hermsues@gmail.com>
2021-08-04 16:07:42 +0300

    ramen menu

---
+++ 05.react-project-1/src/components/layouts/RamenMenu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const RamenMenu = () => {
+	return (
+		<div>
+			<h2>Genuine Japanese Ramen:</h2>
+			<ul>
+				<li>
+					<a href="#!">Text</a>
+				</li>
+				<li>
+					<a href="#!">Text</a>
+				</li>
+				<li>
+					<a href="#!">Text</a>
+				</li>
+				<li>
+					<a href="#!">Text</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default RamenMenu;
