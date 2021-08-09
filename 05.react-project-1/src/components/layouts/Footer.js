f7cf7f939fa57a41e2d40f94c2994442efcdd4c3
yavorpetkov <hermsues@gmail.com>
2021-08-09 10:59:14 +0300

    Footer

--- 05.react-project-1/src/components/layouts/Footer.js
+++ 05.react-project-1/src/components/layouts/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <div>
+            <div className='container'>
                 <ul>
                     <li>
                         <a href="#!">Us</a>
